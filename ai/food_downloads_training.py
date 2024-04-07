0b4812984f7e46349bf327b9b2fb87fa82821f49
rrcoder0167 <riddhiman.rana@gmail.com>
2024-04-07 03:05:28 -0700

    Update image download limit in food_downloads_training.py

--- ai/food_downloads_training.py
+++ ai/food_downloads_training.py
@@ -1,11 +1,7 @@
 from bing_image_downloader import downloader
 
 def download_images(query):
-    downloader.download(query, limit=100,  output_dir='/Users/riddhiman.rana/Downloads/images', adult_filter_off=False, force_replace=False, timeout=60)
+    downloader.download(query, limit=500,  output_dir='/Users/riddhiman.rana/Desktop/Coding/chomp/ai/training_data', adult_filter_off=False, force_replace=False, timeout=60)
 
 download_images("Coca Cola can")
-download_images("Doritos")
-download_images("Orange fruit")
 download_images("Seven Up Can")
-download_images("Sprite Can")
-download_images("Oreos")
